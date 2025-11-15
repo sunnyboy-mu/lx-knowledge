aa7847c4f1902c41022dec5f78b39771fe054d9e
小沐沐吖 <sunnyboy_mu@163.com>
2025-11-15 15:50:05 +0800

    feat: 评论

--- .vuepress/theme.ts
+++ .vuepress/theme.ts
@@ -128,17 +128,14 @@ export default plumeTheme({
    * 评论 comments
    * @see https://theme-plume.vuejs.press/guide/features/comments/
    */
-  // comment: {
-  //   provider: '', // "Artalk" | "Giscus" | "Twikoo" | "Waline"
-  //   comment: true,
-  //   repo: '',
-  //   repoId: '',
-  //   category: '',
-  //   categoryId: '',
-  //   mapping: 'pathname',
-  //   reactionsEnabled: true,
-  //   inputPosition: 'top',
-  // },
+  comment: {
+    provider: "Giscus", // "Artalk“ | "Giscus" | "Twikoo" | "Waline"
+    comment: true,
+    repo: "sunnyboy-mu/lx-knowledge",
+    repoId: "R_kgDOQMLe0g",
+    category: "Announcements",
+    categoryId: "DIC_kwDOQMLe0s4Cx0Ep",
+  },
 
   /**
    * 资源链接替换
